8e304b31f1327a27da57e90c5a86b8c4299ca5fb
Dirk Breuer <dirk.breuer@gmail.com>
2010-03-24 16:10:06 +0100

    We are back to the ThreadedPoller

--- script/listen.rb
+++ script/listen.rb
@@ -3,7 +3,7 @@ puts '=> Loading Workling...'
 
 require File.dirname(__FILE__) + '/../lib/workling/remote'
 require File.dirname(__FILE__) + '/../lib/workling/remote/invokers/basic_poller'
-# require File.dirname(__FILE__) + '/../lib/workling/remote/invokers/threaded_poller'
+require File.dirname(__FILE__) + '/../lib/workling/remote/invokers/threaded_poller'
 require File.dirname(__FILE__) + '/../lib/workling/remote/invokers/eventmachine_subscriber'
 require File.dirname(__FILE__) + '/../lib/workling/routing/class_and_method_routing'
 
